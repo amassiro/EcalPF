77ee30e791fef5745083189ac282948307a070a5
amassiro <massironi.andrea@gmail.com>
2018-02-09 13:46:51 +0100

    test

--- test/draw/drawDeta.cxx
+++ test/draw/drawDeta.cxx
@@ -8,9 +8,21 @@ void drawDeta(){
   
   TGraph* gr = new TGraph();
   for (int i=0; i<40; i++) {
-    float theta = 0;
     
-    float deta = 0;
+    //---- 26 mm 
+    float xtal = 0.026;
+    
+    float y1 = 1.290 - xtal * (i);
+    float y2 = 1.290 - xtal * (i+1);
+    float x = 3.170;
+    
+    float theta1 = atan(y1/x);
+    float theta2 = atan(y2/x);
+    
+    float eta1 = -ln(tan( theta1 / 2.) );
+    float eta2 = -ln(tan( theta2 / 2.) );
+    
+    float deta = eta2- eta1;
     
     gr->SetPoint (i, i, deta);
   }
