9ee0dedb3d072aa97a36c847e185f4609750846c
amassiro <massironi.andrea@gmail.com>
2018-11-02 14:14:48 +0100

    up

--- test/README.md
+++ test/README.md
@@ -286,9 +286,6 @@ Run
     r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"seed_energy\",50,0,50,\"seed_number\>\=61200\"\)
     r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"seed_energy\",20,0,20,\"seed_number\<61200\"\)
         
-    r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"Sum$\(seed_energy\>0\)\",100,0,1000,\"seed_number\<61200\"\)
-    r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"Sum$\(seed_energy\>0\)\",100,0,1000,\"seed_number\>\=61200\"\)
-    
     r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"Sum$\(seed_energy\>0\&\&seed_number\<61200\)\",100,0,1000,\"1\"\)
     r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"Sum$\(seed_energy\>0\&\&seed_number\>\=61200\)\",100,0,1000,\"1\"\)
     r00t  PhoStat_1_numEvent1000.root NoPhoStat_1_numEvent1000.root    draw/drawComparison.cxx\(\"Sum$\(seed_energy\>0\&\&seed_number\>\=61200\)\",50,0,250,\"1\"\)
--- test/draw/drawComparison.cxx
+++ test/draw/drawComparison.cxx
@@ -1,4 +1,45 @@
 
+void SetErrors (TH1F* histo){
+  int nbin = histo->GetNbinsX();
+  for (int iBin=0; iBin < nbin; iBin++) {
+    histo->SetBinError   (iBin, sqrt(histo->GetBinContent(iBin)) );
+  }
+}
+
+
+float SumQ(float A, float B) {
+  return sqrt(A*A + B*B);
+}
+  
+
+TH1F* Ratio (TH1F* hNum, TH1F* hDen) {
+  
+  int nbin = hNum->GetNbinsX();
+  float min = hNum->GetBinLowEdge(1);
+  float max = hNum->GetBinLowEdge(nbin+1);
+  
+  std::cout << " nbin, min, max = " << nbin << " , " << min << " , " << max << std::endl;
+  TString name = Form ("h_ratio_%s_OVER_%s", hNum->GetTitle(), hDen->GetTitle() );
+  
+  TH1F* new_histo = new TH1F (name.Data(), name.Data(), nbin, min, max );
+  
+  for (int iBin=0; iBin < nbin; iBin++) {
+    float ratio = 1.;
+    float error_ratio = 0.;
+    if (hDen->GetBinContent (iBin) != 0) {
+      ratio = hNum->GetBinContent (iBin) / hDen->GetBinContent (iBin);
+      error_ratio = SumQ (hNum->GetBinError (iBin) / hDen->GetBinContent (iBin), hDen->GetBinError (iBin) / hDen->GetBinContent (iBin) * ratio );
+    }
+    
+    new_histo->SetBinContent (iBin, ratio);
+    new_histo->SetBinError   (iBin, error_ratio );
+  }
+  
+  return new_histo;
+}
+
+
+
 void drawComparison(std::string variable, int nbin, float min, float max, std::string cut = "1") { 
 
   std::cout << " variable = " << variable << std::endl;
@@ -24,6 +65,12 @@ void drawComparison(std::string variable, int nbin, float min, float max, std::s
   TH1F* h0 = (TH1F*) gDirectory->Get("h0");
   TH1F* h1 = (TH1F*) gDirectory->Get("h1");
   
+  SetErrors(h0);
+  SetErrors(h1);
+  
+  h0->Sumw2();
+  h1->Sumw2();
+  
   h0->Scale (1./h0->Integral());
   h1->Scale (1./h1->Integral());
   
@@ -46,6 +93,16 @@ void drawComparison(std::string variable, int nbin, float min, float max, std::s
   leg -> AddEntry (h1,    "No  #gamma stat", "PL");
   leg -> Draw();
   
+  
+  TH1F* h_ratio = Ratio (h0, h1);
+  
+  TCanvas* cc = new TCanvas ("cc", "Ratio", 800, 600);
+  h_ratio -> SetLineColor(kBlack);
+  h_ratio -> SetLineWidth(2);
+  h_ratio->GetXaxis()->SetTitle(variable.c_str());
+  h_ratio->GetYaxis()->SetTitle("Ratio Yes/No #gamma");
+  h_ratio->GetYaxis()->SetRangeUser(0.,2.);
+  h_ratio -> Draw();
   
 }
 
